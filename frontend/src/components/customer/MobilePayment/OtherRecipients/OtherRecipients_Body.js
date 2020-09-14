a327ce4c382b287aa744cbad2c0a52974080a699
Zafirah <40818473+zafirahrosley@users.noreply.github.com>
2020-09-14 14:32:14 +0800

    Add files via upload

---
+++ frontend/src/components/customer/MobilePayment/OtherRecipients/OtherRecipients_Body.js
@@ -0,0 +1,191 @@
+import React, { useState, useEffect } from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import "../css/OneTimeTransfer.css";
+import Grid from "@material-ui/core/Grid";
+import TextField from "@material-ui/core/TextField";
+import Select from "@material-ui/core/Select";
+import MenuItem from "@material-ui/core/MenuItem";
+import FormControl from "@material-ui/core/FormControl";
+import Button from "@material-ui/core/Button";
+import { store } from "../../../../index";
+import { useDispatch } from "react-redux";
+import { useHistory } from "react-router-dom";
+import { storeInput } from "../../../redux/actions/mobilePayment_storeInput";
+import axios from "axios";
+
+
+export default function BodyContainer() {
+  const dispatch = useDispatch();
+  const state = store.getState();
+  const history = useHistory();
+  console.log(state);
+
+  const [amount, setAmount] = useState("");
+  const [creditCard, setCreditCard] = useState({});
+  const [isLoading, setLoading] = useState(false);
+  const [visible, setVisible] = useState("hidden");
+  const [nextButton, setNextButton] = useState(false);
+  const [cards, setcards] = useState([]);
+  let id = sessionStorage.getItem('_id');
+
+  useEffect(() => {
+      axios
+        .post("https://bankapp-backend.herokuapp.com/creditcard/cust/searchActive", {
+          userId: id
+        })
+        .then((response) => {
+          setcards(response.data.creditcard);
+          setLoading(true);
+        })
+        .catch((error) => console.log(error));
+    });
+
+  const handleAmount = (value) => {
+    if (value > creditCard.creditcard_balance) {
+      setVisible("visible");
+      setNextButton(true);
+    } else {
+      setVisible("hidden");
+      setNextButton(false);
+    }
+  };
+
+  const handleCreditCard = (e) => {
+    cards.map((creditcard) => {
+      if (creditcard.creditcard_type === e.target.value) {
+        setCreditCard(creditcard);
+        if (amount > creditcard.creditcard_balance) {
+          setVisible("visible");
+          setNextButton(true);
+        } else {
+          setVisible("hidden");
+          setNextButton(false);
+        }
+      }
+    });
+    console.log(creditCard);
+  };
+
+  const handleFormInputs = () => {
+    if (amount != "" && Object.keys(creditCard).length != 0) {
+      dispatch(
+        storeInput(state.mobilePayment.payeeInfo.number, amount, creditCard)
+      );
+      history.push("/Customer/MobilePayment/ConfirmationPage");
+    } else alert("Please fill in the form");
+  };
+
+  const useStyles = makeStyles((theme) => ({
+    appBarSpacer: theme.mixins.toolbar,
+
+    gridMargin: {
+      marginTop: theme.spacing(1),
+    },
+
+    textBoxMargin: {
+      marginTop: theme.spacing(0),
+    },
+
+    formControl: {
+      marginTop: theme.spacing(1),
+      minWidth: 240,
+    },
+
+    errorMessage: {
+      visibility: visible,
+    },
+
+    nextButton: {
+      marginTop: "100px",
+      width: "260px",
+      backgroundColor: "#e26448",
+      color: "white",
+      fontWeight: "bold",
+      "&:hover": {
+        backgroundColor: "#e26448",
+      },
+      fontSize: "1.25em"
+    },
+  }));
+
+  const classes = useStyles();
+
+  const formTo = (
+    <div className="toForm">
+      <h1>To</h1>
+      <div>
+        <p>{state.mobilePayment.payeeInfo.name}</p>
+        <p>Mobile Bill</p>
+      </div>
+      <div>
+        <TextField
+          required
+          className={classes.textBoxMargin}
+          id="amountInput"
+          label="Amount"
+          type="number"
+          value={amount}
+          onChange={(e) => {
+            setAmount(e.target.value);
+            handleAmount(e.target.value);
+          }}
+        />
+      </div>
+    </div>
+  );
+
+  const formFrom = (
+    <div className="fromForm">
+      <h1>From</h1>
+      <div>
+        <FormControl variant="filled" className={classes.formControl}>
+          <Select
+            labelId="selectCreditCard"
+            id="selectCreditCard"
+            onChange={(e) => handleCreditCard(e)}
+          >
+            {cards.map((obj) => (
+              <MenuItem value={obj.creditcard_type}>
+                {obj.creditcard_type}
+              </MenuItem>
+            ))}
+          </Select>
+        </FormControl>
+      </div>
+      <p>Current Balance</p>
+      <p>${creditCard.creditcard_balance}</p>
+      <p className={classes.errorMessage + " errorMessage"}>
+        Please type in an amount less than the balance amount
+      </p>
+      <Button
+        className={classes.nextButton}
+        variant="contained"
+        onClick={() => handleFormInputs()}
+        disabled={nextButton}
+      >
+        Next
+      </Button>
+    </div>
+  );
+
+  return (
+    <main className="content">
+      <Grid container direction="row" justify="space-evenly" wrap="wrap">
+        <Grid item sm={10} className={classes.gridMargin + " billPayment"}>
+          <h1>Bill Payment</h1>
+        </Grid>
+        <Grid
+          item
+          sm={6}
+          direction="row"
+          className={classes.gridMargin + " border"}
+        >
+          {formTo}
+        </Grid>
+        <Grid item sm={6} className={classes.gridMargin + " border"}>
+          {formFrom}
+        </Grid>
+      </Grid>
+    </main>
+  );
+}
---
+++ frontend/src/components/customer/MobilePayment/OtherRecipients/OtherRecipients_PayeeList.js
@@ -0,0 +1,93 @@
+import React, { useState, useEffect } from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import "../css/OtherRecipients.css";
+import Grid from "@material-ui/core/Grid";
+import Paper from "@material-ui/core/Paper";
+import InfoIcon from "@material-ui/icons/Info";
+import Button from "@material-ui/core/Button";
+import axios from "axios";
+import { useDispatch } from "react-redux";
+import { storePayee } from "../../../redux/actions/mobilePayment_storeInput";
+import { useHistory, Link, NavLink } from "react-router-dom";
+
+var payeeList = [];
+
+export default function BodyContainer() {
+  const dispatch = useDispatch();
+  const history = useHistory();
+  const [isLoading, setLoading] = useState(false);
+  let id = sessionStorage.getItem('_id');
+
+  useEffect(() => {
+    const fetchData = () => {
+      axios
+        .get(`https://bankapp-backend.herokuapp.com/payee/mobile/${id}`)
+        .then((response) => {
+          console.log(response);
+          payeeList = response.data.payee;
+          setLoading(true);
+        })
+        .catch((error) => console.log(error));
+    };
+    fetchData();
+  });
+
+  const useStyles = makeStyles((theme) => ({
+    appBarSpacer: theme.mixins.toolbar,
+
+    gridMargin: {
+      marginTop: theme.spacing(1),
+    },
+
+    textBoxMargin: {
+      marginTop: theme.spacing(5),
+    },
+
+    formControl: {
+      marginTop: theme.spacing(8),
+      minWidth: 240,
+    },
+  }));
+
+  const classes = useStyles();
+
+  const handleOnClick = (event) => {
+    payeeList.map((obj) => {
+      if (obj._id === event.target.id) {
+        dispatch(storePayee(obj));
+      }
+    });
+    history.push("/Customer/MobilePayment/OtherRecipients/Form");
+  };
+
+  return (
+    <div>
+      <Grid container direction="row" justify="space-evenly" wrap="wrap">
+        <Grid item sm={10} className={classes.gridMargin + " billPayment"}>
+          <h1>Bill Payment</h1>
+        </Grid>
+        <Grid item sm={12}>
+          {payeeList.map((obj) => (
+            <Paper
+              elevation={3}
+              id={obj._id}
+              className="paperHeight"
+              onClick={(e) => handleOnClick(e)}
+            >
+              <span>{obj.name}</span>
+              <br />
+              <br />
+              <span>Mobile Bill</span>
+              <InfoIcon className="infoIcon" />
+            </Paper>
+          ))}
+        </Grid>
+      </Grid>
+      <a href="/Customer/MobilePayment/AddPayee">
+        <Button id="addPayee" variant="contained">
+          Add Payee
+        </Button>
+      </a>
+    </div>
+  );
+}
