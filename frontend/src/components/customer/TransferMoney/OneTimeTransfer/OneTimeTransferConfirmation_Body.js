04162817f90db7c93c4b9b106183869ec7659456
Zafirah <40818473+zafirahrosley@users.noreply.github.com>
2020-09-14 15:26:59 +0800

    Add files via upload

---
+++ frontend/src/components/customer/TransferMoney/OneTimeTransfer/OneTimeTransferConfirmation_Body.js
@@ -0,0 +1,122 @@
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import "../css/OneTimeTransfer.css";
+import Grid from "@material-ui/core/Grid";
+import Button from "@material-ui/core/Button";
+import { store } from "../../../../index";
+import { useHistory } from "react-router-dom";
+import axios from "axios";
+
+export default function BodyContainer() {
+  const history = useHistory();
+  const state = store.getState();
+  console.log("store: " + state);
+  console.log("cc id:" + state.transfer.creditCard._id);
+  const handleSubmit = () => {
+    axios
+      .post("https://bankapp-backend.herokuapp.com/payment_history/addPayment", {
+        payment_type: "Transfer",
+        payment_amount: state.transfer.amount,
+        transfer_number: 1,
+        creditcardId: state.transfer.creditCard._id,
+      })
+      .then((response) => {
+        console.log(response);
+        axios
+          .patch("https://bankapp-backend.herokuapp.com/creditcard/updateBalance", {
+            creditcardId: state.transfer.creditCard._id,
+            creditcard_balance:
+              state.transfer.creditCard.creditcard_balance -
+              state.transfer.amount,
+          })
+          .then((response2) => {
+            console.log(response2);
+            window.location.href = "/Customer/Payment/Successful";
+          })
+          .catch((error) => {
+            console.log(error);
+            window.location.href = "/Customer/Payment/Unsuccessful";
+          });
+      })
+      .catch((error) => {
+        console.log(error);
+        window.location.href = "/Customer/Payment/Unsuccessful";
+      });
+  };
+
+  const useStyles = makeStyles((theme) => ({
+    appBarSpacer: theme.mixins.toolbar,
+
+    gridMargin: {
+      marginTop: theme.spacing(1),
+    },
+
+    textBoxMargin: {
+      marginTop: theme.spacing(5),
+    },
+
+    formControl: {
+      marginTop: theme.spacing(8),
+      minWidth: 240,
+    },
+  }));
+
+  const classes = useStyles();
+
+  const formTo = (
+    <div className="toForm toFormConfirmation">
+      <h1>To</h1>
+      <p>Reference Number: {state.transfer.payeeInfo.number}</p>
+      <p>$ {state.transfer.amount}</p>
+    </div>
+  );
+
+  const formFrom = (
+    <div className="fromForm fromFormConfirmation">
+      <h1>From</h1>
+      <p>{state.transfer.creditCard.creditcard_type}</p>
+      <div>
+        <Button
+          id="submitButton"
+          variant="contained"
+          onClick={() => handleSubmit()}
+        >
+          Submit
+        </Button>
+      </div>
+      <div>
+        <Button
+          id="cancelButton"
+          variant="contained"
+          onClick={() =>
+            (window.location.href = "/Customer/Payment/Unsuccessful")
+          }
+        >
+          Cancel
+        </Button>
+      </div>
+    </div>
+  );
+
+  return (
+    <main className="content">
+      <div className={classes.appBarSpacer} />
+      <Grid container direction="row" justify="space-evenly" wrap="wrap">
+        <Grid item sm={10} className={classes.gridMargin + " billPayment"}>
+          <h1>Bill Payment Details</h1>
+        </Grid>
+        <Grid
+          item
+          sm={6}
+          direction="row"
+          className={classes.gridMargin + " border"}
+        >
+          {formTo}
+        </Grid>
+        <Grid item sm={6} className={classes.gridMargin + " border"}>
+          {formFrom}
+        </Grid>
+      </Grid>
+    </main>
+  );
+}
---
+++ frontend/src/components/customer/TransferMoney/OneTimeTransfer/OneTimeTransfer_Body.js
@@ -0,0 +1,216 @@
+import React, { useState, useEffect } from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import "../css/OneTimeTransfer.css";
+import Grid from "@material-ui/core/Grid";
+import TextField from "@material-ui/core/TextField";
+import Select from "@material-ui/core/Select";
+import MenuItem from "@material-ui/core/MenuItem";
+import FormControl from "@material-ui/core/FormControl";
+import Button from "@material-ui/core/Button";
+import { useDispatch } from "react-redux";
+import { storeInput } from "../../../redux/actions/transferMoney_storeInput";
+import { store } from "../../../../index";
+import { useHistory, Link } from "react-router-dom";
+import axios from "axios";
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch,
+  NavLink,
+} from "react-router-dom";
+
+export default function BodyContainer() {
+  const dispatch = useDispatch();
+  console.log(store.getState());
+  const history = useHistory();
+
+  const handleFormInputs = () => {
+    if (
+      amount != "" &&
+      Object.keys(creditCard).length != 0 &&
+      accountNumber != ""
+    ) {
+      dispatch(storeInput(accountNumber, amount, creditCard, name));
+      history.push("/Customer/TransferMoney/ConfirmationPage");
+    } else alert("Please fill in the form");
+  };
+
+  const [accountNumber, setAccountNumber] = useState("");
+  const [amount, setAmount] = useState("");
+  const [name, setName] = useState("");
+  const [creditCard, setCreditCard] = useState({});
+  const [isLoading, setLoading] = useState(false);
+  const [visible, setVisible] = useState("hidden");
+  const [nextButton, setNextButton] = useState(false);
+  const [cards, setCards] = useState([]);
+  let id = sessionStorage.getItem("_id");
+
+  useEffect(() => {
+    axios
+      .post("https://bankapp-backend.herokuapp.com/creditcard/cust/searchActive", {
+        userId: id,
+      })
+      .then((response) => {
+        setCards(response.data.creditcard);
+        setLoading(true);
+      })
+      .catch((err) => console.log(err));
+  }, [cards]);
+
+  const handleAmount = (value) => {
+    if (value > creditCard.creditcard_balance) {
+      setVisible("visible");
+      setNextButton(true);
+    } else {
+      setVisible("hidden");
+      setNextButton(false);
+    }
+  };
+
+  const handleCreditCard = (e) => {
+    cards.map((creditcard) => {
+      if (creditcard.creditcard_type === e.target.value) {
+        setCreditCard(creditcard);
+        if (amount > creditcard.creditcard_balance) {
+          setVisible("visible");
+          setNextButton(true);
+        } else {
+          setVisible("hidden");
+          setNextButton(false);
+        }
+      }
+    });
+    console.log(creditCard);
+  };
+
+  const useStyles = makeStyles((theme) => ({
+    appBarSpacer: theme.mixins.toolbar,
+
+    gridMargin: {
+      marginTop: theme.spacing(1),
+    },
+
+    textBoxMargin: {
+      marginTop: theme.spacing(5),
+    },
+
+    formControl: {
+      marginTop: theme.spacing(8),
+      minWidth: 240,
+    },
+
+    errorMessage: {
+      visibility: visible,
+    },
+
+    nextButton: {
+      marginTop: "100px",
+      width: "260px",
+      backgroundColor: "#e26448",
+      color: "white",
+      fontWeight: "bold",
+      "&:hover": {
+        backgroundColor: "#e26448",
+      },
+      fontSize: "1.25em",
+    },
+  }));
+
+  const classes = useStyles();
+
+  const formTo = (
+    <div className="toForm">
+      <h1>To</h1>
+      <div>
+        <TextField
+          required
+          className={classes.textBoxMargin}
+          id="nameInput"
+          label="Name"
+          onChange={(e) => setName(e.target.value)}
+        />
+      </div>
+      <div>
+        <Select native="true" className={classes.textBoxMargin}>
+          <option>Optimum DigiBank</option>
+        </Select>
+      </div>
+      <div>
+        <TextField
+          required
+          className={classes.textBoxMargin}
+          id="phoneNumberInput"
+          label="Account Number"
+          type="number"
+          onChange={(e) => setAccountNumber(e.target.value)}
+        />
+      </div>
+      <div>
+        <TextField
+          required
+          className={classes.textBoxMargin}
+          id="amountInput"
+          label="Amount"
+          type="number"
+          value={amount}
+          onChange={(e) => {
+            setAmount(e.target.value);
+            handleAmount(e.target.value);
+          }}
+        />
+      </div>
+    </div>
+  );
+
+  const formFrom = (
+    <div className="fromForm">
+      <h1>From</h1>
+      <div>
+        <FormControl variant="filled" className={classes.formControl}>
+          <Select
+            labelId="selectCreditCard"
+            id="selectCreditCard"
+            onChange={(e) => handleCreditCard(e)}
+          >
+            {cards.map((obj) => (
+              <MenuItem value={obj.creditcard_type}>
+                {obj.creditcard_type}
+              </MenuItem>
+            ))}
+          </Select>
+        </FormControl>
+      </div>
+      <p>Current Balance</p>
+      <p>${creditCard.creditcard_balance}</p>
+      <p className={classes.errorMessage + " errorMessage"}>
+        Please type in an amount less than the balance amount
+      </p>
+      <Button
+        className={classes.nextButton}
+        variant="contained"
+        onClick={() => handleFormInputs()}
+        disabled={nextButton}
+      >
+        Next
+      </Button>
+    </div>
+  );
+  return (
+    <Grid container direction="row" justify="space-evenly" wrap="wrap">
+      <Grid item sm={10} className={classes.gridMargin + " billPayment"}>
+        <h1>Bill Payment</h1>
+      </Grid>
+      <Grid
+        item
+        sm={6}
+        direction="row"
+        className={classes.gridMargin + " border"}
+      >
+        {formTo}
+      </Grid>
+      <Grid item sm={6} className={classes.gridMargin + " border"}>
+        {formFrom}
+      </Grid>
+    </Grid>
+  );
+}
